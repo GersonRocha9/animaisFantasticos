5605669755f6bdfcd409804955de5ddd80a97c47
GersonRocha9 <gersonrocha9@gmail.com>
2021-09-11 15:41:20 -0300

    Exercícios das Aulas

--- script.js
+++ script.js
@@ -36,3 +36,34 @@
 // });
 
 // imgs.forEach(() => i++);
+
+// const menu = document.querySelector(".menu");
+
+// Lista todas as classes em formato de String
+// console.log(menu.className);
+
+// menu.classList.add("ativo");
+// add = adicionar
+// remove = remove
+// toggle = adiciona (se não tiver) ou remove (se tiver)
+
+// const menuAtivo = document.querySelectorAll(".menu a");
+// menuAtivo.forEach((item) => item.classList.add("ativo"));
+
+// menuAtivo.forEach((item) => item.classList.remove("ativo"));
+
+// menuAtivo[0].classList.add("ativo");
+
+// const imgs = document.querySelectorAll("img");
+// console.log(imgs);
+
+// imgs.forEach((img) => {
+//   const possuiAlt = img.hasAttribute("alt");
+//   console.log(possuiAlt);
+// });
+
+// const hrefExterno = document.querySelectorAll('.menu [href^="https"]');
+
+// hrefExterno.forEach((item) => {
+//   item.setAttribute("href", "https://www.linkedin.com/gersonrocha");
+// });
