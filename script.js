99aaade6a024130513d3b035a1c7a3453d2a33ba
GersonRocha9 <gersonrocha9@gmail.com>
2021-09-11 14:52:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// // const imagens = document.querySelectorAll("img");
+// // console.log(imagens);
+
+// // const palavraImagem = document.querySelectorAll('img[src ^= "img/imagem"]');
+// // console.log(palavraImagem);
+
+// // const linksInternos = document.querySelectorAll('[href^="#"]');
+// // console.log(linksInternos);
+
+// // const subtitulo = document.querySelector("h2");
+// // console.log(subtitulo);
+
+// // const ultimoParagrafo = document.querySelectorAll("p");
+// // console.log(ultimoParagrafo[ultimoParagrafo.length - 1]);
+
+// // const imagens = document.querySelectorAll("img");
+// // imagens.forEach((item) => console.log(item));
+
+// const paragrafos = document.querySelectorAll("p");
+
+// console.log(paragrafos);
+
+// paragrafos.forEach((item) => console.log(item.innerText));
+
+// // -------------------------
+
+// const imgs = document.querySelectorAll("img");
+
+// imgs.forEach((item, index) => {
+//   console.log(item, index);
+// });
+
+// let i = 0;
+// imgs.forEach(() => {
+//   console.log(i++);
+// });
+
+// imgs.forEach(() => i++);
